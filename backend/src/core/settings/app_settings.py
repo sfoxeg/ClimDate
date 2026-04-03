8dbc38c0735ebdc05d66c694b85199857f1510fb
Sega <sfoxig@ya.ru>
2026-04-03 21:38:36 +0300

    add settings from .env

--- backend/src/core/settings/app_settings.py
+++ backend/src/core/settings/app_settings.py
@@ -1,7 +1,11 @@
+import os
 from pydantic import BaseModel
+from ipaddress import IPv4Address
 
 
 class AppSettings(BaseModel):
-    host: str = "127.0.0.1"
-    port: int = 8000
-    reload: bool = True
+    __ip: IPv4Address = IPv4Address(os.environ.get("HOST", "127.0.0.1"))
+    host: str = str(__ip)
+    port: int = int(os.environ.get("PORT", 8000))
+    reload: bool = bool(os.environ.get("RELOAD", False))
+    log_level: str = os.environ.get("LOG_LEVEL", "INFO")
