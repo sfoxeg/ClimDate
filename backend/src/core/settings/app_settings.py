e89c2bf74107301d354bc114dbf05ebefbdef611
Sega <sfoxig@ya.ru>
2026-04-03 22:07:09 +0300

    fix app host setting

--- backend/src/core/settings/app_settings.py
+++ backend/src/core/settings/app_settings.py
@@ -1,11 +1,9 @@
 import os
 from pydantic import BaseModel
-from ipaddress import IPv4Address
 
 
 class AppSettings(BaseModel):
-    __ip: IPv4Address = IPv4Address(os.environ.get("HOST", "127.0.0.1"))
-    host: str = str(__ip)
+    host: str = os.environ.get("HOST", "127.0.0.1")
     port: int = int(os.environ.get("PORT", 8000))
     reload: bool = bool(os.environ.get("RELOAD", False))
     log_level: str = os.environ.get("LOG_LEVEL", "INFO")
